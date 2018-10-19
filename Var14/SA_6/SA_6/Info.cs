158ee7bb25dd8296ee4cc162ff671b9322946d33
Anton <lalaka32@mail.ru>
2018-10-19 10:49:19 +0300

    6 только для 2-х кластеров

---
+++ Var14/SA_6/SA_6/Info.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SA_6
+{
+    class Info
+    {
+        public Info(double price, double mark)
+        {
+            Price = price;
+            Mark = mark;
+        }
+
+        public double Price { get; set; }
+        public double Mark { get; set; }
+    }
+}
